8a0abfb7869be3648264c0bffe5d0235a67023fd
Abdul Rehman <abdul@pulsemusic.com>
2025-05-12 14:56:42 +0400

    Initail commit

---
+++ Sources/MyMaths.swift
@@ -0,0 +1,12 @@
+//
+//  MyMaths.swift
+//  MyLibraryPckage
+//
+//  Created by Abdul Rehman on 12/05/2025.
+//
+
+class MyMaths {
+    func sum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
