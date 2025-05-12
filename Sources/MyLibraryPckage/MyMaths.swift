034182c1e9de247d7a9fb51e7eb493e667b0792a
Abdul Rehman <abdul@pulsemusic.com>
2025-05-12 15:12:42 +0400

    adding new class

---
+++ Sources/MyLibraryPckage/MyMaths.swift
@@ -0,0 +1,16 @@
+//
+//  MyMaths.swift
+//  MyLibraryPckage
+//
+//  Created by Abdul Rehman on 12/05/2025.
+//
+
+public class MyMaths {
+    
+    public init() {
+        
+    }
+    public func sum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
